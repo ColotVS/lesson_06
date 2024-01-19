36f1126ae67d834dff0ac228c3d40e3acd895238
ColotVS <kolotilov98@mail.ru>
2024-01-19 15:06:17 +0700

    Инициализация массива Урок 6, задача 1

---
+++ lesson_06_01.java
@@ -0,0 +1,8 @@
+public class lesson_06_01 {
+    public static void main(String[] args) {
+        //В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов,
+        // размер которых равен 10.
+        int[] arrayInt = new int[10];
+        double[] arrayDouble = new double[10];
+    }
+}
